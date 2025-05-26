fe5716a62c196b6d771b2692ca4bfb11d137b7ec
Anibal Sanchez <anibalnuma@gmail.com>
2025-05-26 00:02:20 -0400

    Implemented Lazy Loading for components. Use textarea for description field in Card form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,50 @@
 import { Routes } from '@angular/router';
-import { AfterAuthComponent } from './core/components/after-auth/after-auth.component';
-import { LoginComponent } from './core/components/login/login.component';
-import { TrelloAccountComponent } from './core/components/trello-account/trello-account.component';
-import { TrelloBoardComponent } from './core/components/trello-board/trello-board.component';
-import { TrelloCardComponent } from './core/components/trello-card/trello-card.component';
 import { authGuard } from './core/guards/auth.guard';
-import { RedirectComponent } from './core/components/redirect/redirect.component';
-import { DashboardComponent } from './core/components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: RedirectComponent,
+    loadComponent: () =>
+      import('./core/components/redirect/redirect.component').then(
+        (m) => m.RedirectComponent
+      ),
   },
   {
     path: 'login',
-    component: LoginComponent,
+    loadComponent: () =>
+      import('./core/components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
   },
   {
     path: 'after-auth',
-    component: AfterAuthComponent,
+    loadComponent: () =>
+      import('./core/components/after-auth/after-auth.component').then(
+        (m) => m.AfterAuthComponent
+      ),
   },
   {
     path: 'dashboard',
-    component: DashboardComponent,
     canActivate: [authGuard],
+    loadComponent: () =>
+      import('./core/components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
   },
   {
     path: 'trello-board/:id',
-    component: TrelloBoardComponent,
     canActivate: [authGuard],
+    loadComponent: () =>
+      import('./core/components/trello-board/trello-board.component').then(
+        (m) => m.TrelloBoardComponent
+      ),
   },
   {
     path: 'trello-card/:id',
-    component: TrelloCardComponent,
     canActivate: [authGuard],
+    loadComponent: () =>
+      import('./core/components/trello-card/trello-card.component').then(
+        (m) => m.TrelloCardComponent
+      ),
   },
 ];
